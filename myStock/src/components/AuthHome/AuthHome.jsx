eb2d98ff3263e66f97937f564d85910883d21078
williamwallace076 <wallacewilliam076@gmail.com>
2023-08-07 18:58:55 -0300

    feat:login_page_added

--- myStock/src/components/AuthHome/AuthHome.jsx
+++ myStock/src/components/AuthHome/AuthHome.jsx
@@ -5,13 +5,19 @@ const AuthHome = () => {
     return ( 
         <div className='AuthHome'>
             <div className='Area-auth'>
+                <div className='title-auth-area'>
+                <h1>Faça login na sua conta <span>MyStack </span>!</h1>
+                </div>
                 <div className='Auth-inputs-area'>
                     <label htmlFor="input-email">Email</label>
                     <input id='input-email' type="email" placeholder='Useremail@.com.br'/>
                     <label htmlFor="input-pass">Senha</label>
                     <input id='input-pass' type="password" placeholder='Password'/>
                 </div>
-                <button className='bt-Auth'>Login</button>
+                <div className='area-bt-auth'>
+                    <button className='bt-Auth'>Login</button>
+                    <span>Ainda não tem cadastro ? <b>Cadastre-se aqui !</b></span>
+                </div>
             </div>
         </div>
      );
