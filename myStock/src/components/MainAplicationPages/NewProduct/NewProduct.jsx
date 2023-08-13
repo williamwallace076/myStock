111d6344e63f4f0fe3d4af6c5c3a00d1600f09c3
williamwallace076 <wallacewilliam076@gmail.com>
2023-08-13 16:44:14 -0300

    feat:page_cadProd_modified

--- myStock/src/components/MainAplicationPages/NewProduct/NewProduct.jsx
+++ myStock/src/components/MainAplicationPages/NewProduct/NewProduct.jsx
@@ -4,8 +4,26 @@ import React from 'react';
 
 const NewProduct = () => {
     return ( 
-        <div>
-new
+        <div className="newProduct">
+
+        <h2 className="title-newProduct">Cadastrar Novo Produto</h2>
+
+        <div className="newProduct-area">
+            <form className="form-newProduct" action="">
+                <label htmlFor="name">Nome do Produto</label>
+                <input id="name" className="name-product" type="text" />
+                <label htmlFor="desc">Descrição do produto</label>
+                <input id="desc" className="description-product" type="text" />
+                <label htmlFor="quanty">Quantidade</label>
+                <input id="quanty" className="quantity" type="number" />
+                <label htmlFor="price">Preço do Produto</label>
+                <input id="price" className="price" type="text" />
+
+
+                <button type="">Cadastrar novo Produto</button>
+            </form>
+        </div>
+
         </div>
      );
 }
