3a94058d990769af31cc51a9ee2cc2e91d907cae
williamwallace076 <wallacewilliam076@gmail.com>
2024-01-13 17:03:38 -0300

    modules_alter

--- myStock/src/App.jsx
+++ myStock/src/App.jsx
@@ -2,10 +2,7 @@ import './App.css';
 import Routes from './Routes';
 import Footer from './components/MainAplicationPages/Footer/Footer';
 import Header from './components/MainAplicationPages/Header/Header';
-import Home from './components/MainAplicationPages/Home/Home';
 import Sidebar from './components/MainAplicationPages/SideBar/SideBar';
-import AuthHome from './components/authHome/AuthHome';
-import Register from './components/register/Register';
 import ConponentProvider from './contexts/Statecomponents';
 
 import { BrowserRouter } from 'react-router-dom';
