d01f5af6548b7e8f1a0f03474463a0b454dedd79
williamwallace076 <wallacewilliam076@gmail.com>
2024-04-16 16:54:46 -0300

    alter_sidebar

--- src/components/MainAplicationPages/SideBar/SideBar.jsx
+++ src/components/MainAplicationPages/SideBar/SideBar.jsx
@@ -9,9 +9,9 @@ const Sidebar = () => {
                 <h2 className="title-sidebar">My Stack</h2>
                 <nav className="nav-menu">
                     <a href="Home" className="nav-item-sidebar">Inicio</a>
+                    <a href="Sell" className="nav-item-sidebar">Nova Venda</a>
                     <a href="Stock" className="nav-item-sidebar">Estoque</a>
                     <a href="NewProduct" className="nav-item-sidebar">Cadastrar <br /> Produto</a>
-                    <a href="Sell" className="nav-item-sidebar">Nova Venda</a>
                 </nav>
             </div>
         </div>
